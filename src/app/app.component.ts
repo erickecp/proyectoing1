dd39f53afa36e80f5d77ad03d3d28b460903d3c7
Leslie-Viv <84611239+Leslie-Viv@users.noreply.github.com>
2023-05-31 19:36:38 -0600

    Agregando el Service de alert

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertServiceService } from './services/alert-service.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'proyecto1ng';
+
+  constructor(private alertService: AlertServiceService){}
+
+  //mandar a llamar el service y la creacion del metodo y se manda a llamar el metodo global
+  trigger(message:string, action:string){
+    this.alertService.openSnackBar(message, action);
+   }
 }
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { AlertServiceService } from './services/alert-service.service';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -13,8 +18,12 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
     BrowserModule,
     BrowserAnimationsModule,
     MatSlideToggleModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSnackBarModule
   ],
-  providers: [],
+  providers: [AlertServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
